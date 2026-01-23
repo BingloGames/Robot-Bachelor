aca9894c9815a11f9fae466212742c4b55806fe6
Binglo <58050557+BingloGames@users.noreply.github.com>
2026-01-23 14:07:00 +0100

    Stars, robot

---
+++ robot-project/start_counter.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+var star_count = 0
+var full_star = preload("res://star full.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		new_start()
+
+func new_start():
+	star_count += 1
+	get_node("HBoxContainer/"+str(star_count)+"/Sprite2D").texture = full_star

3c5266384cc89b38e868c3ee65b0ed9158826126
LisaCabot <lisacabot@gmail.com>
2026-01-27 20:55:37 +0100

    Changed size of star and question button. Implemented level 5 and 4 with what we have.

--- robot-project/item_list.gd
+++ robot-project/item_list.gd
@@ -1,6 +1,6 @@
 extends ItemList
 @export var itemsList = ["forward()", "backward()", "left()", "right()", "for i in range(n)" ,"X = ", "Y = ", "List = []"]
-var items = 2
+var items = 8
 var function = ""
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,23 +19,85 @@ func _process(delta: float) -> void:
 
 #Button 1:
 func _on_button_1_mouse_entered() -> void:
-	get_node("/root/Node2D/Container/Button1/Label1").show()
+	get_node("/root/Node2D/Container/Text").set_text("Text for forward()")
+	get_node("/root/Node2D/Container/Text").show()
 	pass # Replace with function body.
 
 func _on_button_1_mouse_exited() -> void:
-	get_node("/root/Node2D/Container/Button1/Label1").hide()
+	get_node("/root/Node2D/Container/Text").hide()
 	pass # Replace with function body.
 
 #Button 2: 
 func _on_button_2_mouse_entered() -> void:
-	get_node("/root/Node2D/Container/Button2/Label2").show()
+	get_node("/root/Node2D/Container/Text").set_text("Text for backward()")
+	get_node("/root/Node2D/Container/Text").show()
 	pass # Replace with function body.
 
 func _on_button_2_mouse_exited() -> void:
-	get_node("/root/Node2D/Container/Button2/Label2").hide()
+	get_node("/root/Node2D/Container/Text").hide()
 	pass # Replace with function body.
 
+#Button 3:
+func _on_button_3_mouse_entered() -> void:
+	get_node("/root/Node2D/Container/Text").set_text("Text for left()")
+	get_node("/root/Node2D/Container/Text").show()
+	pass # Replace with function body.
 
+func _on_button_3_mouse_exited() -> void:
+	get_node("/root/Node2D/Container/Text").hide()
+	pass # Replace with function body.
+
+#Button 4:
+func _on_button_4_mouse_entered() -> void:
+	get_node("/root/Node2D/Container/Text").set_text("Text for right()")
+	get_node("/root/Node2D/Container/Text").show()
+	pass # Replace with function body.
+
+func _on_button_4_mouse_exited() -> void:
+	get_node("/root/Node2D/Container/Text").hide()
+	pass # Replace with function body.
+
+#Button 5:
+func _on_button_5_mouse_entered() -> void:
+	get_node("/root/Node2D/Container/Text").set_text("Text for for i in range(n)")
+	get_node("/root/Node2D/Container/Text").show()
+	pass # Replace with function body.
+
+func _on_button_5_mouse_exited() -> void:
+	get_node("/root/Node2D/Container/Text").hide()
+	pass # Replace with function body.
+
+#Button 6:
+func _on_button_6_mouse_entered() -> void:
+	get_node("/root/Node2D/Container/Text").set_text("Text for X = ")
+	get_node("/root/Node2D/Container/Text").show()
+	pass # Replace with function body.
+
+func _on_button_6_mouse_exited() -> void:
+	get_node("/root/Node2D/Container/Text").hide()
+	pass # Replace with function body.
+
+#Button 7:
+func _on_button_7_mouse_entered() -> void:
+	get_node("/root/Node2D/Container/Text").set_text("Text for Y = ")
+	get_node("/root/Node2D/Container/Text").show()
+	pass # Replace with function body.
+
+func _on_button_7_mouse_exited() -> void:
+	get_node("/root/Node2D/Container/Text").hide()
+	pass # Replace with function body.
+
+#Button 8:
+func _on_button_8_mouse_entered() -> void:
+	get_node("/root/Node2D/Container/Text").set_text("Text for List = []")
+	get_node("/root/Node2D/Container/Text").show()
+	pass # Replace with function body.
+
+func _on_button_8_mouse_exited() -> void:
+	get_node("/root/Node2D/Container/Text").hide()
+	pass # Replace with function body.
+
+#Function so when we click on a function from the item list, it gets pasted in to the coding window:
 func _on_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
 	function = str(itemsList[index]) + "\n"
 	get_node("/root/Node2D/TextEdit").text += function

6fabd96210650a065b9bc1e35b244869aea3b8e1
LisaCabot <lisacabot@gmail.com>
2026-02-06 12:29:10 +0100

    updated item_list, now it wont paste if the last line is not empty

--- robot-project/item_list.gd
+++ robot-project/item_list.gd
@@ -41,7 +41,15 @@ func _on_button_1_mouse_exited() -> void:
 
 #Button 2: 
 func _on_button_2_mouse_entered() -> void:
-	get_node("Text").set_text("Text for backward()")
+	item = get_node("ItemList").get_item_text(1)
+	var file_path = path + language + item + ".txt"
+	if FileAccess.file_exists(file_path):
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		if file: 
+			var content = file.get_as_text()
+			get_node("Text").set_text(content)
+		else: 
+			print("Error opening file: ", file)
 	get_node("Text").show()
 	pass # Replace with function body.
 
@@ -51,7 +59,15 @@ func _on_button_2_mouse_exited() -> void:
 
 #Button 3:
 func _on_button_3_mouse_entered() -> void:
-	get_node("Text").set_text("Text for left()")
+	item = get_node("ItemList").get_item_text(2)
+	var file_path = path + language + item + ".txt"
+	if FileAccess.file_exists(file_path):
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		if file: 
+			var content = file.get_as_text()
+			get_node("Text").set_text(content)
+		else: 
+			print("Error opening file: ", file)
 	get_node("Text").show()
 	pass # Replace with function body.
 
@@ -61,7 +77,15 @@ func _on_button_3_mouse_exited() -> void:
 
 #Button 4:
 func _on_button_4_mouse_entered() -> void:
-	get_node("Text").set_text("Text for right()")
+	item = get_node("ItemList").get_item_text(3)
+	var file_path = path + language + item + ".txt"
+	if FileAccess.file_exists(file_path):
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		if file: 
+			var content = file.get_as_text()
+			get_node("Text").set_text(content)
+		else: 
+			print("Error opening file: ", file)
 	get_node("Text").show()
 	pass # Replace with function body.
 
@@ -71,7 +95,15 @@ func _on_button_4_mouse_exited() -> void:
 
 #Button 5:
 func _on_button_5_mouse_entered() -> void:
-	get_node("Text").set_text("Text for for i in range(n):")
+	item = get_node("ItemList").get_item_text(4)
+	var file_path = path + language + item + ".txt"
+	if FileAccess.file_exists(file_path):
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		if file: 
+			var content = file.get_as_text()
+			get_node("Text").set_text(content)
+		else: 
+			print("Error opening file: ", file)
 	get_node("Text").show()
 	pass # Replace with function body.
 
@@ -81,7 +113,15 @@ func _on_button_5_mouse_exited() -> void:
 
 #Button 6:
 func _on_button_6_mouse_entered() -> void:
-	get_node("Text").set_text("Text for X = ")
+	item = get_node("ItemList").get_item_text(5)
+	var file_path = path + language + item + ".txt"
+	if FileAccess.file_exists(file_path):
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		if file: 
+			var content = file.get_as_text()
+			get_node("Text").set_text(content)
+		else: 
+			print("Error opening file: ", file)
 	get_node("Text").show()
 	pass # Replace with function body.
 
@@ -91,7 +131,15 @@ func _on_button_6_mouse_exited() -> void:
 
 #Button 7:
 func _on_button_7_mouse_entered() -> void:
-	get_node("Text").set_text("Text for Y = ")
+	item = get_node("ItemList").get_item_text(6)
+	var file_path = path + language + item + ".txt"
+	if FileAccess.file_exists(file_path):
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		if file: 
+			var content = file.get_as_text()
+			get_node("Text").set_text(content)
+		else: 
+			print("Error opening file: ", file)
 	get_node("Text").show()
 	pass # Replace with function body.
 
@@ -101,7 +149,15 @@ func _on_button_7_mouse_exited() -> void:
 
 #Button 8:
 func _on_button_8_mouse_entered() -> void:
-	get_node("Text").set_text("Text for List = []")
+	item = get_node("ItemList").get_item_text(7)
+	var file_path = path + language + item + ".txt"
+	if FileAccess.file_exists(file_path):
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		if file: 
+			var content = file.get_as_text()
+			get_node("Text").set_text(content)
+		else: 
+			print("Error opening file: ", file)
 	get_node("Text").show()
 	pass # Replace with function body.
 
@@ -114,10 +170,17 @@ func _on_item_clicked(index: int, at_position: Vector2, mouse_button_index: int)
 	function = str(itemsList[index]) + "\n"
 	var line_limit = get_node("/root/Node2D/code").line_limit
 	var current_line = get_node("/root/Node2D/code/TextEdit").get_line_count()
-	if current_line >= line_limit:
+	if current_line > line_limit:
 		print("more code lines than line limit")
 		return
+		
+	if current_line == line_limit:
+		var line = get_node("/root/Node2D/code/TextEdit").get_line(current_line-1)
+		if line == "":
+			get_node("/root/Node2D/code/TextEdit").text += function
+			
 	else:
 		get_node("/root/Node2D/code/TextEdit").text += function
+		
 	pass # Replace with function body.
 	# Example usage, assuming TextEdit node is named "MyTextEdit"

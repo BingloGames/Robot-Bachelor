4ac5c094b0c7ddf414bb6743bfe5ce7fadb3828b
LisaCabot <lisacabot@gmail.com>
2026-02-09 12:29:28 +0100

    fixed pathing info pop up and item list info

--- robot-project/info_pop_up.gd
+++ robot-project/info_pop_up.gd
@@ -1,11 +1,17 @@
 extends Control
-@onready var path = Global.text_path
-@onready var language = Global.text_language
-var no_players = "1"
-@onready var current_level = Global.current_level
-var item = no_players + " player" + "/Level" + current_level
+var path = ""
+var language = ""
+var no_players = ""
+var current_level = ""
+var item = ""
+var file_path = ""
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var path = Global.text_path
+	var language = Global.text_language
+	var no_players = "1"
+	var current_level = str(Global.current_level)
+	var item = no_players + " player" + "/Level" + current_level
 	var file_path = path + language + item + ".txt"
 	if FileAccess.file_exists(file_path):
 		var file = FileAccess.open(file_path, FileAccess.READ)

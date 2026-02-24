a6bbedd34649a8106f3bcdff43665b224cb3c3ab
Binglo <sebba.helgeland@gmail.com>
2026-02-24 16:50:06 +0100

    Can create and connect with server

--- robot-project/2_player_menu.gd
+++ robot-project/2_player_menu.gd
@@ -6,32 +6,35 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_host_pressed() -> void:
 	get_node("host game").show()
 	get_node("layer 1").hide()
 	
 	var interfaces = IP.get_local_interfaces()
 	
+	
 	for interface in interfaces:
-		if not interface["friendly"] == "Wi-Fi":
+		if not (interface["friendly"] == "Wi-Fi" or interface["friendly"] == "Ethernet"):
 			continue
 		
 		
-		print("Wi-Fi address: ", interface["addresses"])
-		
-		
+		#print("address: ", interface["addresses"])
 		var temp_text = "Your IP addresses: \n"
 		for address in interface["addresses"]:
 			temp_text += address + "\n"
 		
 		
-		#temp_text = temp_text.left(-1)
 		get_node("host game/ip address").text = temp_text
+	
+	
+	var player_name = get_node("name selector").text
+	if player_name == "":
+		print("you need a name")
+		# do so the player sees error
+		return
+	
+	
+	ConnectionController.host_game(player_name)
 
 
 func _on_join_pressed() -> void:
@@ -40,8 +43,23 @@ func _on_join_pressed() -> void:
 
 
 func _on_connect_pressed() -> void:
-	pass
-	#try connecting using given ip address
+	var ip_address = get_node("join game/ip address").text
+	var player_name = get_node("name selector").text
+	
+	
+	if player_name == "":
+		print("you need a name")
+		# do so the player sees error
+		return
+	
+	
+	if not ip_address.is_valid_ip_address():
+		print("invalid ip address")
+		# do so the player sees error
+		return
+	
+	
+	ConnectionController.join_game(ip_address, player_name)
 
 
 func _on_start_pressed() -> void:
@@ -66,3 +84,8 @@ func _on_host_back_pressed() -> void:
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://start_menu.tscn")
+
+
+func _on_name_changed(new_text: String) -> void:
+	pass
+	#if player is connected, change name if it is valid
---
+++ robot-project/connection_controller.gd
@@ -0,0 +1,58 @@
+extends Node
+
+
+const PORT = 3032
+
+
+var peer = null
+var player_name = "Robot"
+
+
+var players = {}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	multiplayer.peer_connected.connect(_player_connected)
+	multiplayer.peer_disconnected.connect(_player_disconnected)
+	multiplayer.connection_failed.connect(_connection_failed)
+
+
+func _connection_failed():
+	print("failed connection")
+	multiplayer.multiplayer_peer = null
+	#tell the player that the connection failed
+
+
+func _player_connected(id):
+	register_player.rpc_id(id, player_name)
+	#allow the host to start the game
+
+
+func _player_disconnected(id):
+	players.erase(id)
+	#visually remove the disconnected player
+	#if the game is ongoing, stop the game
+
+
+
+@rpc("any_peer")
+func register_player(new_player_name):
+	var id = multiplayer.get_remote_sender_id()
+	players[id] = new_player_name
+	print("player with name: ", new_player_name, " added!")
+	#update visually that the player is added
+
+
+func host_game(new_player_name):
+	player_name = new_player_name
+	peer = ENetMultiplayerPeer.new()
+	peer.create_server(PORT, 2)
+	multiplayer.set_multiplayer_peer(peer)
+
+
+func join_game(ip_address, new_player_name):
+	player_name = new_player_name
+	peer = ENetMultiplayerPeer.new()
+	peer.create_client(ip_address, PORT)
+	multiplayer.set_multiplayer_peer(peer)

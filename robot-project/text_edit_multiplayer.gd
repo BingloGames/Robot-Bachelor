6ab1ddbc89aa74cfe38fb5bafe3c3ea7cf7452bb
LisaCabot <lisacabot@gmail.com>
2026-02-26 12:12:56 +0100

    robots now walk in multiplayer yay

--- robot-project/text_edit_multiplayer.gd
+++ robot-project/text_edit_multiplayer.gd
@@ -4,11 +4,6 @@ extends "res://text_edit.gd"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if multiplayer.is_server():
-		robot = get_node("r://oot/Node2D/robot1")
+		robot = get_node("/root/Node2D/robot1")
 	else:
-		robot = get_node("://root/Node2D/robot2")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		robot = get_node("/root/Node2D/robot2")

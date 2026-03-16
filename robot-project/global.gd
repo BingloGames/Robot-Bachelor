50ad5532bbe0790fd90994457da9cddd97d1ca2b
LisaCabot <lisacabot@gmail.com>
2026-03-16 20:18:10 +0100

    changed size of some scenes, made levels 3-5 multiplayer, fixed some global positions

--- robot-project/global.gd
+++ robot-project/global.gd
@@ -29,7 +29,7 @@ func save_stars(star_count: int) -> void:
 
 func restart_level() -> void:
 	print("resetting level!")
-	get_node("/root/Node2D").robots_finished.clear()
+	#get_node("/root/Node2D").robots_finished.clear()
 	get_node("/root/Node2D/code").problem_warning()
 	get_node("/root/Node2D/code").stop_running_code()
 	

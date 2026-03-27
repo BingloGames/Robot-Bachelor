1081cd3df4629166b43619836c7a5aadc57454cd
LisaCabot <lisacabot@gmail.com>
2026-03-27 13:15:23 +0100

    item list multiplayer made and updated in all 5 levels

---
+++ robot-project/item_list_multiplayer.gd
@@ -0,0 +1,8 @@
+extends "res://item_list.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	super._ready()
+	itemsList = ["forward()", "backward()", "wait()", "left()", "right()", "for i in range(n):"]
+	pass # Replace with function body.

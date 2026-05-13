9b1dab38411406e7964876f495d45ad8dc21b80a
Binglo <sebba.helgeland@gmail.com>
2026-05-13 18:49:54 +0200

    Text files

--- robot-project/components/singleplayer/UI/back button/back.gd
+++ robot-project/components/singleplayer/UI/back button/back.gd
@@ -7,6 +7,7 @@ var player_menu_file = "res://menus/1_player_menu.tscn"
 
 ##Changes the scene to the single player player menu file.
 func back():
+	get_tree().paused = false
 	get_tree().change_scene_to_file(player_menu_file)
 
 func _on_back_pressed() -> void:
